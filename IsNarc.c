af4adcfbb1447742b9964fc1cdf1ed9750163a95
ImKool-Gitch <71771827+ImKool-Gitch@users.noreply.github.com>
2022-09-05 13:08:26 +0200

    Create IsNarc.c

---
+++ IsNarc.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+bool isnarc(char* str)
+{
+    int res = 0;
+    for (size_t i = 0; i < strlen(str); ++i)
+    {
+        res += pow((str[i] - '0'), strlen(str));
+    }
+    if (res != atoi(str)) return false;
+    else return true;
+}
+
+int main()
+{
+    char num[500];
+    printf("Enter a number: ");
+    scanf("%s", num);
+    if (isnarc(num)) printf("The number %d is a narcissist number\n", atoi(num));
+    else printf("The number %d is not a narcissist number\n", atoi(num));
+    return 0;
+}
